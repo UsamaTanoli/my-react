ae5fc665daf307b59e5f071e97b5f241060c8723
UsamaTanoli <usamakhan123451998@gmail.com>
2023-09-27 01:35:24 +0500

    counter app

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(1)
+
+  function increment() {
+    if (count < 20) {
+      setCount(count + 1)
+    }
+  }
+
+
+  function decrement() {
+    if (count > 0) {
+      setCount(count - 1);
+
+    }
+  }
+
+
+  return (
+
+    <>
+      <h1>counter = {count}</h1>
+      <button onClick={increment}>increase</button>
+      <br />
+      <br />
+      <button onClick={decrement}>decrease</button>
+    </>
+  )
+}
+
+export default App
